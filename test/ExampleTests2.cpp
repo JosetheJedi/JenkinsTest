ec4f76497bb47c7f9bd7331831dfd07e8e80a6c3
Jose Hernandez-Uribe <josefhu15@gmail.com>
2020-05-20 14:13:52 -0700

    added second unit test

---
+++ test/ExampleTests2.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "../Example.hpp"
+
+TEST(SecondTests, firstTestCase)
+{
+    ASSERT_TRUE(true);
+}
+
+TEST(SecondTests, secondTestCase)
+{
+    ASSERT_TRUE(true);
+
+    Example ex = Example();
+}
