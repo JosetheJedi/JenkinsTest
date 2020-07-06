75f5a84deb050d318970a3d61fcb10b5b1994742
Jose Hernandez-Uribe <josefhu15@gmail.com>
2020-07-06 12:25:50 -0700

    he

--- test.sh
+++ test.sh
@@ -7,5 +7,3 @@ read -p "Give me a commit message: " commitMessage
 git commit -m "${commitMessage}"
 
 git log --oneline
-
-osascript -e "display notification 'pushed to remote' with title 'SUCCESS'"
